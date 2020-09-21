a08716596ed09253c71553c251828b9546d3b7fc
jutem <chendawei322@gmail.com>
2020-09-21 09:03:23 +0800

    push

--- src/main/java/com/jutem/cases/leetcode/LongestPalindromicSubstring.java
+++ src/main/java/com/jutem/cases/leetcode/LongestPalindromicSubstring.java
@@ -1,8 +1,47 @@
 package com.jutem.cases.leetcode;
 
+import sun.jvm.hotspot.gc_implementation.parallelScavenge.PSYoungGen;
+
 /**
  * @author dawei.chen02@ele.me
  * @create 2020-09-20
  */
 public class LongestPalindromicSubstring {
+
+
+    //abbc bb
+    public static String longestPalindrome(String s) {
+        if (s.length() == 1 || s.length() == 0) {
+            return s;
+        }
+
+        String subStr = "";
+        for (float i = 0.5f; i <= s.length() - 1.5; i += 0.5) {
+
+            int left = (int) Math.ceil(i - 1), right = (int) Math.floor(i + 1);
+            for (; left >= 0 && right < s.length(); left--, right++) {
+                if (s.charAt(left) != s.charAt(right)) {
+                    break;
+                }
+            }
+
+            String str = s.substring(left + 1, right);
+            if (str.length() > subStr.length()) {
+                subStr = str;
+            }
+        }
+
+        if (subStr.length() == 0) {
+            return s.substring(0, 1);
+        }
+
+        return subStr;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(longestPalindrome("a"));
+        System.out.println("12345678".substring(0, 8));
+        System.out.println(Math.ceil(1) + " : " + Math.floor(1.5));
+
+    }
 }
--- src/main/java/com/jutem/cases/leetcode/MedianOfTwoSortedArrays.java
+++ src/main/java/com/jutem/cases/leetcode/MedianOfTwoSortedArrays.java
@@ -1,8 +1,5 @@
 package com.jutem.cases.leetcode;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * @author dawei.chen02@ele.me
  * @create 2020-09-17
@@ -13,6 +10,31 @@ public class MedianOfTwoSortedArrays {
         System.out.printf(findMedianSortedArrays(new int[]{1, 2}, new int[]{3, 4}) + "");
     }
 
+    //另一种思路:寻找中位数，可以转换成寻找第k小的数字，二分法效率更好
+
+    //归并的思路,更优雅的写法
+    public static double findMedianSortedArraysBetter(int[] A, int[] B) {
+        int m = A.length;
+        int n = B.length;
+        int len = m + n;
+        int left = -1, right = -1;
+        int aStart = 0, bStart = 0;
+        for (int i = 0; i <= len / 2; i++) {
+            left = right;
+            //大前提可以保证数组A取不到的情况下，数组B一定能取到
+            if (aStart < m && (bStart >= n || A[aStart] < B[bStart])) {
+                right = A[aStart++];
+            } else {
+                right = B[bStart++];
+            }
+        }
+        if ((len & 1) == 0)
+            return (left + right) / 2.0;
+        else
+            return right;
+    }
+
+    //归并的思路
     public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
         int allLength = nums1.length + nums2.length;
 
