b439cb17669e3ac820bff139178fa8b333287afc
jutem <chendawei322@gmail.com>
2020-10-02 17:19:28 +0800

    add nice symmetric

--- src/main/java/com/jutem/cases/structure/Tree.java
+++ src/main/java/com/jutem/cases/structure/Tree.java
@@ -209,4 +209,20 @@ public class Tree {
         return false;
     }
 
+    /**
+     * 镜像，更优雅的做法
+     */
+    public boolean isSymmetricNice(TreeNode root) {
+        return check(root, root);
+    }
+    public boolean check(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p == null || q == null) {
+            return false;
+        }
+        return p.val == q.val && check(p.left, q.right) && check(p.right, q.left);
+    }
+
 }
