5e9e2e352d6f0807e36f68b65bf7872cd09fa6ef
jutem <chendawei322@gmail.com>
2021-02-21 16:18:12 +0800

    push

---
+++ src/main/java/com/jutem/cases/leetcode/MaxSizeSlices.java
@@ -0,0 +1,22 @@
+package com.jutem.cases.leetcode;
+
+/**
+ * @author dawei.chen02@ele.me
+ * @create 2021-02-21
+ *
+ * 3n 块披萨
+ *
+ * 给你一个披萨，它由 3n 块不同大小的部分组成，现在你和你的朋友们需要按照如下规则来分披萨：
+ *
+ * 你挑选 任意 一块披萨。
+ * Alice 将会挑选你所选择的披萨逆时针方向的下一块披萨。
+ * Bob 将会挑选你所选择的披萨顺时针方向的下一块披萨。
+ * 重复上述过程直到没有披萨剩下。
+ * 每一块披萨的大小按顺时针方向由循环数组 slices 表示。
+ *
+ * 请你返回你可以获得的披萨大小总和的最大值。
+ *
+ * 链接：https://leetcode-cn.com/leetbook/read/dynamic-programming-1-plus/5rlibc/
+ */
+public class MaxSizeSlices {
+}
